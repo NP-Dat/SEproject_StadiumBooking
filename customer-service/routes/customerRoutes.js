db6b710beb0e290894417f749d0977306bc0c67b
quagghyyIU <luongquanghuy592@gmail.com>
2025-02-28 02:56:26 +0700

    Revert "Be/huy"

--- customer-service/routes/customerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const customerController = require('../controllers/customerController');
-
-router.post('/register', customerController.register);
-router.post('/login', customerController.login);
-
-module.exports = router;
