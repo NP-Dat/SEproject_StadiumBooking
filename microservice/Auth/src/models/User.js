9d0c2be8f56dc68089e25e86235ef886e20109cf
NP-Dat <128621978+NP-Dat@users.noreply.github.com>
2025-03-04 23:12:41 +0700

    test login/register, connect DB online, fix bypass bug

--- microservice/Auth/src/models/User.js
+++ microservice/Auth/src/models/User.js
@@ -3,8 +3,13 @@ const bcrypt = require('bcryptjs');
 
 class UserModel {
   static async findByEmail(email) {
-    const [users] = await pool.query('SELECT * FROM users WHERE email = ?', [email]);
+    const [users] = await pool.query('SELECT * FROM Customers WHERE email = ?', [email]);
     return users[0];
+  }
+
+  static async findPasswordByEmail(email) {
+    const [passwords] = await pool.query('SELECT passWord FROM Customers WHERE email = ?', [email]);
+    return passwords.length > 0 ? passwords[0].passWord : null;
   }
 
   static async findById(id) {
@@ -15,8 +20,8 @@ class UserModel {
   static async create(username, email, password) {
     const hashedPassword = await bcrypt.hash(password, 10);
     const [result] = await pool.query(
-      'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
-      [username, email, hashedPassword]
+      'INSERT INTO Customers (userName, email, passWord, fullName, birth, phoneNumber, address) VALUES (?, ?, ?, ?, ?, ?, ?)',
+      [username, email, hashedPassword,"Dat", "2000-01-01", "0123456789", "HCM"]
     );
     return result.insertId;
   }
