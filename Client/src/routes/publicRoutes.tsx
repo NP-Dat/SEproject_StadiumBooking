e3fd46bbbf66f9d41a5a027987de9b0634afa345
Thai Quang Tinh <tinhthai2000@gmail.com>
2025-04-23 11:04:49 +0700

    Complete V2

--- Client/src/routes/publicRoutes.tsx
+++ Client/src/routes/publicRoutes.tsx
@@ -1,11 +1,22 @@
 import { RouteType } from '../types/routes'
 import Home from '../pages/Home/Home'
 import About from '../pages/About/About'
+import Venues from '../pages/Venues/Venues'
 
 export const publicRoutes: RouteType[] = [
   {
     path: '/',
     element: <Home />
+  },
+
+  /*{
+    path: '/',
+    element: <Event />
+  }*/
+
+  {
+    path: '/venues',
+    element: <Venues />
   },
   {
     path: '/about',
