18a9de6df48557960969d61712a7dcd798557fb3
duythucne22 <duythuc332@gmail.com>
2025-05-01 18:58:51 +0700

    Event Service handle events and schedules

--- Server/microservice/Event/src/routes/eventRoutes.js
+++ Server/microservice/Event/src/routes/eventRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const eventController = require('../controllers/eventController');
+const { authenticateToken, authorizeOwner } = require('../middleware/authMiddleware');
 const router = express.Router();
-const EventController = require('../controllers/eventController');
-const { authenticateToken } = require('../middleware/authMiddleware');
 
-// Public routes
-router.get('/', EventController.getEvents);
-router.get('/:id', EventController.getEvent);
+// Public routes (no authentication required)
+router.get('/', eventController.getAllEvents);
+router.get('/:id', eventController.getEventById);
 
-// Protected routes
-router.post('/', authenticateToken, EventController.createEvent);
-router.put('/:id', authenticateToken, EventController.updateEvent);
-router.delete('/:id', authenticateToken, EventController.deleteEvent);
+// Protected routes (require authentication)
+// router.post('/', authenticateToken, authorizeOwner, eventController.createEvent);
+// router.put('/:id', authenticateToken, authorizeOwner, eventController.updateEvent);
+// router.delete('/:id', authenticateToken, authorizeOwner, eventController.deleteEvent);
 
 module.exports = router;
